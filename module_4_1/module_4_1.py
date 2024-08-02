ad87b35221a0be76f7a7ae56d2f91f219e8f8fea
BEV001 <122723865+BEV001@users.noreply.github.com>
2024-08-02 17:22:32 +0300

    Add files via upload

---
+++ module_4_1/module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as school_divide
+from true_math import divide as univ_divide
+
+result1 = school_divide(69, 3)
+result2 = school_divide(3, 0)
+result3 = univ_divide(49, 7)
+result4 = univ_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ module_4_1/true_math.py
@@ -0,0 +1,10 @@
+from math import inf
+
+def divide(first, second):
+
+    if second != 0:
+        result = first / second
+    else:
+        result = inf
+
+    return result
