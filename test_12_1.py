a02f3572c25abe63c3d0267b396b2748d1bc2308
BEV001 <122723865+BEV001@users.noreply.github.com>
2024-11-03 17:23:01 +0300

    Add files via upload

---
+++ test_12_1.py
@@ -0,0 +1,29 @@
+import runner
+import unittest
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        tester = runner.Runner("walking")
+        for i in range(0, 10):
+            runner.Runner.walk(tester)
+        self.assertEqual(tester.distance, 50)
+
+    def test_run(self):
+        tester = runner.Runner("running")
+        for i in range(0, 10):
+            runner.Runner.run(tester)
+        self.assertEqual(tester.distance, 100)
+
+    def test_challenge(self):
+        tester_walk = runner.Runner("walking 1")
+        tester_run = runner.Runner("running 1")
+        for i in range(0, 10):
+            runner.Runner.walk(tester_walk)
+            runner.Runner.run(tester_run)
+        self.assertNotEqual(tester_walk.distance, tester_run.distance)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
