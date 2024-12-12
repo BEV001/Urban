a168a49b35ed7fb07eb0865b7a5e818a07f2c0af
BEV001 <122723865+BEV001@users.noreply.github.com>
2024-12-12 17:56:31 +0300

    Update main.py

--- module_14_3/main.py
+++ module_14_3/main.py
@@ -50,12 +50,12 @@ async def get_buying_list(message):
         i+=1
     await message.answer(text.message_choosing, reply_markup=kb_product)
 
-@dp.message_handler(text="Формула расчёта")
+@dp.message_handler(text='Формула расчёта')
 async def get_formulas(message):
     await message.answer(text.formula_info, reply_markup=main_menu)
 
 
-@dp.message_handler(text="Рассчитать норму калорий")
+@dp.message_handler(text='Рассчитать норму калорий')
 async def set_age(message):
     await message.answer('Введите свой возраст:')
     await UserState.age.set()
@@ -63,30 +63,71 @@ async def set_age(message):
 
 @dp.message_handler(state=UserState.age)
 async def set_growth(message, state):
-    await state.update_data(age = float(message.text))
-    data = await state.get_data()
-    await message.answer('Введите свой рост (см):')
-    await UserState.growth.set()
+    if message.text == 'Назад' or message.text == 'Формула расчета':
+        await message.answer("Возвращаемся в главное меню.",
+                             reply_markup=start_kb)
+        await state.finish()
+        return
+
+    try:
+        age = float(message.text)
+        if 0 < age < 120:
+            await state.update_data(age = age)
+            data = await state.get_data()
+            await message.answer('Введите свой рост (см):')
+            await UserState.growth.set()
+        else:
+            await message.answer('Пожалуйста, введите корректный возраст (от 1 до 119 лет).')
+    except ValueError:
+        await message.answer('Это не похоже на число! Пожалуйста, введите свой возраст числом.')
+
 
 @dp.message_handler(state=UserState.growth)
 async def set_weight(message, state):
-    await state.update_data(growth = float(message.text))
-    data = await state.get_data()
-    await message.answer('Введите свой вес (кг):')
-    await UserState.weight.set()
+    if message.text == 'Назад' or message.text == 'Формула расчета':
+        await message.answer("Возвращаемся в главное меню.",
+                             reply_markup=start_kb)
+        await state.finish()
+        return
+    try:
+        growth = float(message.text)
+        if 0 < growth < 300:
+            await state.update_data(growth = growth)
+            data = await state.get_data()
+            await message.answer('Введите свой вес (кг):')
+            await UserState.weight.set()
+        else:
+            await message.answer('Пожалуйста, введите корректный рост (от 1 до 300 см).')
+    except ValueError:
+        await message.answer('Это не похоже на число! Пожалуйста, введите свой рост числом в см.')
 
 @dp.message_handler(state=UserState.weight)
 async def send_calories(message, state):
-    await state.update_data(weight = float(message.text))
-    data = await state.get_data()
+    if message.text == 'Назад' or message.text == 'Формула расчета':
+        await message.answer("Возвращаемся в главное меню.",
+                             reply_markup=start_kb)
+        await state.finish()
+        return
+    try:
+        weight = float(message.text)
+        if 0 < weight < 500:
+            await state.update_data(weight = weight)
+            data = await state.get_data()
 
-    cal_calories = 10*data['weight']+6.25*data['growth']-5*data['age']+5
-    await message.answer(f'Ваша норма калорий: {cal_calories}', reply_markup=start_kb)
-    await state.finish()
+            cal_calories = 10 * data['weight'] + 6.25 * data['growth'] - 5 * data['age'] + 5
+            await message.answer(f'Ваша норма калорий: {cal_calories}', reply_markup=start_kb)
+            await state.finish()
+
+        else:
+            await message.answer('Пожалуйста, введите корректный вес (от 1 до 500 кг).')
+    except ValueError:
+            await message.answer('Это не похоже на число! Пожалуйста, введите свой вес числом в кг.')
+
 
 
 @dp.callback_query_handler(text='product_buying')
 async def send_confirm_message(call):
+
     await call.message.answer(text.sell)
     await call.answer()
 
