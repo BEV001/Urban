f20da2b25e10ed97511371e5032cefcaf24d7e99
BEV001 <122723865+BEV001@users.noreply.github.com>
2025-01-12 17:04:38 +0300

    Delete module_18/project_2 directory

--- module_18/project_2/task2/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-# Create your views here.
-def index1(request):
-    return render(request, "second_task/index1.html")
-
-class index2(TemplateView):
-    template_name = "second_task/index2.html"
