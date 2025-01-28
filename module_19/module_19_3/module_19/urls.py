f1b2fc3499f3cf29c4941f0a0d9fef45dba7f1f5
BEV001 <122723865+BEV001@users.noreply.github.com>
2025-01-28 17:17:30 +0300

    Add files via upload

---
+++ module_19/module_19_3/module_19/urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for module_19 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from task_2.views import django_sign_up, platform, card, games
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', django_sign_up, name='django_sign_up'),  # можно также добавить имя для homepage
+    path('platform/', platform, name='platform'),
+    path('platform/card/', card, name='card'),
+    path('platform/games/', games, name='games'),
+]
