a7efd256b1d53b5cc60c55e2dad0d9d17913c43b
BEV001 <122723865+BEV001@users.noreply.github.com>
2025-01-30 15:41:32 +0300

    Add files via upload

---
+++ module_19/module_19_5/module_19/task_2/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Game, Buyer, News
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')  # Отображение полей в списке
+    search_fields = ('title',)  # Поиск по полю title
+    list_filter = ('size', 'cost')  # Фильтрация по size и cost
+    list_per_page = 20  # Ограничение количества записей до 20
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')  # Отображение полей в списке
+    search_fields = ('name',)  # Поиск по полю name
+    list_filter = ('balance', 'age')  # Фильтрация по balance и age
+    list_per_page = 30  # Ограничение количества записей до 30
+    readonly_fields = ('balance',)  # Поле balance доступно только для чтения
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date')
+    search_fields = ('title',)
+    list_filter = ('date',)
